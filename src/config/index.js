368109516070a67d24286c2e7629961224d7138e
Ankit Kumar Nayak <68335227+ankitnayak09@users.noreply.github.com>
2023-12-13 19:58:01 +0530

    Add appwriteDatabaseId to config

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,7 @@
 const config = {
 	appwriteUrl: String(import.meta.env.VITE_APP_APPWRITE_URL),
 	appwriteProjectId: String(import.meta.env.VITE_APP_APPWRITE_PROJECT_ID),
+	appwriteDatabaseId: String(import.meta.env.VITE_APP_APPWRITE_DATABASE_ID),
 	appwriteCollectionId: String(
 		import.meta.env.VITE_APP_APPWRITE_COLLECTION_ID
 	),
