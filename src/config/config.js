fcecca8bd800910059a001d0bf1bed62c888933b
Ankit Kumar Nayak <68335227+ankitnayak09@users.noreply.github.com>
2023-12-13 19:23:39 +0530

    Update config.js with environment variables

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,10 @@
-const config = {};
+const config = {
+	appwriteUrl: String(import.meta.env.VITE_APP_APPWRITE_URL),
+	appwriteProjectId: String(import.meta.env.VITE_APP_APPWRITE_PROJECT_ID),
+	appwriteCollectionId: String(
+		import.meta.env.VITE_APP_APPWRITE_COLLECTION_ID
+	),
+	appwriteBucketId: String(import.meta.env.VITE_APP_APPWRITE_BUCKET_ID),
+};
 
 export default config;
